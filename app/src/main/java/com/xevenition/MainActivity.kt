407860b06bcae6a41dc273f94c6fa35e3da79847
Ludwig <bolling.ludwig@gmail.com>
2018-05-29 22:15:40 +0200

    Fixed the google maps key

--- app/src/main/java/com/xevenition/MainActivity.kt
+++ app/src/main/java/com/xevenition/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.xevenition
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import app.bolling.chucknorris.ui.fragment.joke.MapFragment
+import com.xevenition.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -29,6 +28,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        // Add product list fragment if this is first creation
+        if (savedInstanceState == null) {
+            val fragMan = supportFragmentManager
+            val fragTransaction = fragMan.beginTransaction()
+            fragTransaction.replace(binding.frameContent.id, MapFragment()).commit()
+        }
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            if (supportFragmentManager.backStackEntryCount > 0) {
+                //show back arrow
+                supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            } else {
+                supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+            }
+        }
     }
 
     override fun onBackPressed() {
